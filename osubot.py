444ad211ba5c6c0150b7f665b4af87e02112de98
HlebYt1 <56267475+HlebYt1@users.noreply.github.com>
2020-03-28 23:16:42 +0300

    Update osubot.py

--- osubot.py
+++ osubot.py
@@ -4,24 +4,27 @@ import inspect
 import asyncio
 import random
 import os
+import json
+from datetime import datetime
 
 Bot = commands.Bot(command_prefix="osu!")
 Bot.remove_command("help")
 
 @Bot.event
 async def on_ready():
-    print(f"{Bot.user} работает!")
+    print(f"{Bot.user} workk!")
 
 @Bot.command(pass_context= True)
 async def help(ctx):
-    emb = discord.Embed(color=0xf90aff, description="<a:osivisualizer:688403122523209731>Привет я osu!bot мой префикс `osu!` \n Стандартные комманды:\n `say` Сказать что то через бота\n `mute` Выдает мут пользователю\n `help` Показывает это меню\n `unmute` Снимает мут с пользователя\n `kick` Кикнет пользователя\n `minesweeper` Сапер теперь в дискорде\n `info` Информация о боте\n `ping` Проверка пинга бота \n `invite` Пригласить бота к себе на сервер\n `report` Отправить баг репорт создателю\n `updates` Обновления бота\n `8ball` Задай вопрос боту!\n Комманды создателя:\n `crd` Перезагрузка cogs бота\n `eval` Выполнение строк кода")
+    emb = discord.Embed(color=0xf90aff, description="<a:osivisualizer:688403122523209731>Привет я osu!bot бот с кучей команд вот и они:\n Комманды модерации\n `mute` Выдает мут нужны права изменение ролей\n`unmute` Снимает мут с участника нужны права изменение ролей\n `ban` Банит участника права для роли банить участников\n `lock` Выдает роль блокировки права упраление каналами\n `unlock` Снимает блокировку права для роли изменять каналы\n `kick` Кикает участника права для роли кикать участников\n `createblockroom` Создает каналы для заблокированных\n Простые комманды\n `8ball` Обычный шар\n `minesweper` Сапер в дискорде\n `say` Сказать что то через бота\n `info` Информация о боте\n `report` Отправить баг репорт\n `updates` Обновления бота\n Команды создателя\n `crd` Перезапуск cogs\n `shutdown` Отключение бота\n `eval` Выполнение строк кода")
     emb.set_footer(text="osu!bot copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
     await ctx.send(embed = emb)
 
+owners_bot = [521313537943928852, 466310620132802583, 639144156123365386, 477733320583544838]
 def you_user(ctx):
-    return ctx.message.author.id == 521313537943928852
-
-@Bot.command(name='eval', aliases=["e"], pass_context=True)
+    return ctx.message.author.id in owners_bot
+    
+@Bot.command(name='eval', aliases=["e", "vzlomjopi", "durka"], pass_context=True)
 @commands.check(you_user)
 async def eval_(ctx, *, command):
     res = eval(command)
@@ -30,13 +33,8 @@ async def eval_(ctx, *, command):
     else:
         await ctx.send(res)   
 
-@Bot.command(pass_context=False)
-async def ping(ctx):
-    ping = round(Bot.latency * 1000)
-    await ctx.send(f"Пинг: `{ping}ms`")
-
 @Bot.command(pass_context= True)
-@commands.has_permissions(administrator=True)
+@commands.has_permissions(kick_members=True)
 async def kick(ctx, member:discord.Member = None):
     if not member:
         emb = discord.Embed(color=0xf90aff, title = "osu!kick", description="**Укажите кого надо кикнуть!<:redTick:596576672149667840>**")
@@ -56,7 +54,7 @@ async def kick_error(ctx, error):
       await ctx.send(embed = emb)
 
 @Bot.command(pass_context= True)
-@commands.has_permissions(administrator=True)
+@commands.has_permissions(manage_roles=True)
 async def mute(ctx, member: discord.Member=None):
     if not member:
         emb = discord.Embed(color=0xf90aff, title = "osu!mute", description="**Укажите кого надо замутить!<:redTick:596576672149667840>**")
@@ -77,7 +75,7 @@ async def mute_error(ctx, error):
         await ctx.send(embed = emb)
  
 @Bot.command(pass_context= True)
-@commands.has_permissions(administrator=True)
+@commands.has_permissions(manage_roles=True)
 async def unmute(ctx, member: discord.Member=None):
     if not member:
         emb = discord.Embed(color=0xf90aff, title = "osu!unmute", description="**Укажите кого надо размутить!<:redTick:596576672149667840>**")
@@ -96,6 +94,26 @@ async def unmute_error(ctx, error):
         emb = discord.Embed(color=0xf90aff, title = "osu!unmute", description="**Ты не можешь размутить этого пользователя!<:redTick:596576672149667840>**")
         emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
         await ctx.send(embed = emb)
+        
+@Bot.command(pass_context= True)
+@commands.has_permissions(administrator=True)
+async def ban(ctx, member:discord.Member = None):
+    if not member:
+        emb = discord.Embed(color=0xf90aff, title = "osu!ban", description="Укажите кого надо **забанить!**<:redTick:596576672149667840>")
+        emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+        await ctx.send(embed = emb)
+        return
+    await member.ban()
+    emb = discord.Embed(color=0xf90aff, title = "osu!ban", description=f"{member.mention} Был **забанен!**<:greenTick:596576670815879169>**")
+    emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+    await ctx.send(embed = emb)
+
+@ban.error
+async def ban_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+      emb = discord.Embed(color=0xf90aff, title = "osu!ban", description="**Ты не **можешь использовать** эту комманду<:redTick:596576672149667840>**")
+      emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+      await ctx.send(embed = emb)
 
 @Bot.command(pass_context= True)
 async def invite(ctx):
@@ -117,17 +135,16 @@ async def hui(ctx):
     emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
     await ctx.send(embed = emb)
 
-1
 @Bot.command()
 async def say(ctx, *, msg):
     await ctx.message.delete()
     embed = discord.Embed(
         title = "<a:osuvisualizer:688403122523209731>Кто-то что-то сказал...",
-        description = "{}" .format(msg)
+        color=0xf90aff, description = "{}" .format(msg)
     )
     await ctx.send(embed = embed)
 
-initial_extensions = ['status', 'minesweeper'] 
+initial_extensions = ['status', 'minesweeper', 'inter','osu'] 
 
 def reloader(ctx):
     for extension in initial_extensions:
@@ -161,4 +178,69 @@ async def loh(ctx):
     emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
     await ctx.send(embed = emb)
 
+@Bot.command(name= 'shutdown', pass_context=True)
+@commands.is_owner()
+async def lol(ctx):
+    msg = await ctx.send("<a:loadingg:689150668359205064> **see next time**")
+    await asyncio.sleep(5)
+    await msg.edit(content = "<a:loadingg:689150668359205064> **Бот отключен**")
+    await Bot.close()
+
+@Bot.command(name='lock', pass_context= True)
+@commands.has_permissions(manage_channels=True)
+async def lal(ctx, member: discord.Member=None):
+    if not member:
+        emb = discord.Embed(color=0xf90aff, title = "osu!lock", description="**Укажите кого надо заблокировать!<:redTick:596576672149667840>**")
+        emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+        await ctx.send(embed = emb)
+        return
+    emb = discord.Embed(color=0xf90aff, title = "osu!lock", description=f"{member.mention} **Был заблокирован!<:greenTick:596576670815879169>**")
+    emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+    await ctx.send(embed = emb)
+    role = discord.utils.get(ctx.guild.roles, name="Blocked")
+    await member.add_roles(role)
+
+@Bot.command(name='unlock', pass_context= True)
+@commands.has_permissions(manage_roles=True)
+async def lul(ctx, member: discord.Member=None):
+    if not member:
+        emb = discord.Embed(color=0xf90aff, title = "osu!unlock", description="**Укажите кого надо разблокировать!<:redTick:596576672149667840>**")
+        emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+        await ctx.send(embed = emb)
+        return
+    emb = discord.Embed(color=0xf90aff, title = "osu!unlock", description=f"{member.mention} **Был разбокирован!<:greenTick:596576670815879169>**")
+    emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+    await ctx.send(embed = emb)    
+    role = discord.utils.get(ctx.guild.roles, name="Blocked")
+    await member.remove_roles(role)
+
+@Bot.command(name='createblockroom', pass_context=True)
+@commands.has_permissions(administrator=True)
+async def her(ctx):
+    await ctx.guild.create_category('Для заблокированных')
+    await ctx.guild.create_text_channel('чат для блокнутых')
+    await ctx.guild.create_text_channel('бот канал')
+    await ctx.guild.create_text_channel('флудилка')
+    await ctx.guild.create_role(name="Blocked")
+    await ctx.send("Комнаты были созданы, вам нужно только их настроить")
+
+@Bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount = 1000):
+    await ctx.channel.purge(limit=amount)
+    emb = discord.Embed(color=0xf90aff, title = "osu!clear", description=f'{amount} Сообщений было удалено<:greenTick:596576670815879169>')
+    emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+    await ctx.send(embed = emb)    
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+       emb = discord.Embed(color=0xf90aff, title = "osu!clear", description="**Ты не можешь удалять сообщения!<:greenTick:596576670815879169>")
+       emb.set_footer(text="osu!bot Copyright 2020-2020", icon_url="https://media.discordapp.net/attachments/675294482991808513/687743226102546545/lazer.png?width=585&height=585")
+       await ctx.send(embed = emb)
+
+@Bot.command()
+async def ping(ctx):
+    ping = round(Bot.latency * 1000)
+    await ctx.send(f"Пинг бота равен: `{ping}ms`")
+    
 Bot.run("token")
